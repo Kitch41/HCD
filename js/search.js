bcd10012809c3da3bfa543e5927bfd9fa1054b0d
Kitch41 <stefkeuken@hotmail.com>
2024-04-09 09:39:50 +0200

    day 1

--- js/search.js
+++ js/search.js
@@ -1,7 +1,10 @@
-function filterCards() {
+function filterCards(e) {
+    e.preventDefault();
+
     var searchInput = document.getElementById('searchInput').value.trim().toLowerCase();
     var cards = document.querySelectorAll('.cards .cardlink');
-    
+    var visibleCardsCount = 0; // Initialize visible cards count
+
     cards.forEach(function(card) {
         var title = card.querySelector('.titel').innerText.toLowerCase();
         var description = card.querySelector('.description').innerText.toLowerCase();
@@ -12,15 +15,53 @@ function filterCards() {
         var colorMatches = color.split(' ').some(word => word.startsWith(searchInput));
         
         if (titleMatches || descriptionMatches || colorMatches) {
-            card.style.display = 'block';
+            card.style.display = 'block'; // Show the card
+            visibleCardsCount++; // Increment visible cards count
         } else {
-            card.style.display = 'none';
+            card.style.display = 'none'; // Hide the card
         }
     });
+
+    // Get the element you want to show/hide (noitems message)
+    var noItemsMessage = document.getElementById('noitems');
+
+    noItemsMessage.addEventListener("click", clearSearch)
+
+    // Check if there are no visible cards
+    if (visibleCardsCount === 0) {
+        // Display the 'noitems' message
+        noItemsMessage.style.display = 'block';
+    } else {
+        // Hide the 'noitems' message
+        noItemsMessage.style.display = 'none';
+    }
 }
 
+const searchInput = document.getElementById("searchInput");
 
+if (searchInput) {
+    searchInput.addEventListener("input", filterCards);
+} else {
+    console.log("No search bar present");
+}
 
-const submitsearch = document.getElementById("searchButton");
+function clearSearch(e) {
+    e.preventDefault();
 
-submitsearch.addEventListener("click", filterCards)
+    var searchInput = document.getElementById('searchInput');
+    var cards = document.querySelectorAll('.cards .cardlink');
+    var noItemsMessage = document.getElementById('noitems');
+
+    searchInput.value = null;
+
+    cards.forEach(function(card) {
+        card.style.display = "flex";
+    })
+    
+    noItemsMessage.style.display = "none";
+
+    searchInput.focus();
+    
+
+    filterCards()
+}
